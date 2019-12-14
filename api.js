6824ea32db717bdee62591d63ab2999787251a26
Lucas Livchits <livchitsl@gmail.com>
2019-12-14 18:19:52 -0300

    Add request to de API

---
+++ api.js
@@ -0,0 +1,32 @@
+const request = require('request');
+
+function parseJokes(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    const data = JSON.parse(body);
+    // console.log('Hey ->', data.results)
+    return data.results; //en lugar de retornar los datos debería pasárselos a una callback con la lógica de ¿outoput?
+  } else {
+    throw error; //en lugar de enviar el error debería pasarle el error a la callback con la lógica de output
+  }
+}
+
+function getJokes(err, keyword) {
+  if (err) {
+    throw err;
+  }
+
+  const options = {
+    url: `https://icanhazdadjoke.com/search?term=${keyword}`,
+    headers: {
+      Accept: 'application/json'
+    }
+  };
+
+  return request(options, parseJokes);
+}
+
+//return getJokes(keyword)
+
+module.exports = {
+  getJokes
+};
