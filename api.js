3ce4a2060a2a5d75580d4222e7a5577af96ac705
Lucas Livchits <livchitsl@gmail.com>
2019-12-20 10:56:28 -0300

    Remove comments

--- api.js
+++ api.js
@@ -3,19 +3,9 @@ const request = require('request');
 function parseJokes(error, response, body) {
   if (!error && response.statusCode == 200) {
     const data = JSON.parse(body);
-    // console.log('Hey ->', data.results)
-    return data.results; //en lugar de retornar los datos debería pasárselos a una callback con la lógica de ¿outoput?
+    return data.results; //en lugar de retornar los datos debería pasárselos a una callback con la lógica de outoput
   } else {
-    throw error; //en lugar de enviar el error debería pasarle el error a la callback con la lógica de output. De hecho debería lamar primero a la callback con el error, luego a la callback con los datos, algo así
-    /*
-    if (error) {
-      callback(error)
-    } else {
-      const data = JSON.parse(body);
-      callback(null, data.results)
-    }
-
-    */
+    throw error; //en lugar de enviar el error debería pasarle el error a la callback con la lógica de output.
   }
 }
 
@@ -33,8 +23,6 @@ function getJokes(err, keyword) {
 
   request(options, parseJokes);
 }
-
-//return getJokes(keyword)
 
 module.exports = {
   getJokes
