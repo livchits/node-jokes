f4b835de4ff2a9a354b50cbc7003d6c3bc49af09
Lucas Livchits <livchitsl@gmail.com>
2020-01-05 12:53:33 -0300

    Create getMostPopularJoke function and rewrite countJokes

--- leaderboard.js
+++ leaderboard.js
@@ -1,6 +1,16 @@
 function countJokes(jokesArray) {
-  return jokesArray.reduce((acc, joke) => {
+  const countedJokesObject = jokesArray.reduce((acc, joke) => {
     !acc[joke] ? (acc[joke] = 1) : acc[joke]++;
     return acc;
   }, {});
+
+  const countedJokes = Object.entries(countedJokesObject);
+
+  return countedJokes;
+}
+
+function getMostPopularJoke(countedJokesArray) {
+  const sortedJokes = sortJokes(countedJokesArray);
+  const mostPopularJoke = sortedJokes[0][0];
+  return mostPopularJoke;
 }
